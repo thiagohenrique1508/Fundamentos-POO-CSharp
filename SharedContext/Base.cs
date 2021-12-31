98abdfd7b95d7b7acc12cebd31dfcd39e5f3d3f1
Thiago Henrique Martins da Silva <thiagohenrique1508@gmail.com>
2021-12-31 17:08:11 -0300

    Projeto Final

---
+++ SharedContext/Base.cs
@@ -0,0 +1,16 @@
+
+using System;
+using Balta.NotificationContext;
+
+namespace Balta.SharedContext
+{
+    public abstract class Base : Notifiable
+    {
+        public Base()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+    }
+}
